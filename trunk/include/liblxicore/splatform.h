e1c378177105575872bf9aa978748ec63c81bcbf
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2011-04-17 16:14:42 +0000

    Windows fixes.

--- trunk/include/liblxicore/splatform.h
+++ trunk/include/liblxicore/splatform.h
@@ -20,39 +20,36 @@
 #ifndef LXICORE_SPLATFORM_H
 #define LXICORE_SPLATFORM_H
 
-#if defined(Q_OS_UNIX) && defined(__GNUC__)
+#if defined(__unix__) && defined(__GNUC__)
 # define __internal             __attribute__((visibility("hidden")))
 # define __pure                 __attribute__((const))
 # define __packed               __attribute__((packed))
 # define __expect(condition, x) __builtin_expect(condition, x)
 # define __popcount(x)          __builtin_popcount(x)
 
-#elif defined(Q_OS_WIN) && defined(__GNUC__)
+#elif defined(WIN32) && defined(__GNUC__)
 # define __internal
 # define __pure                 __attribute__((const))
 # define __packed               __attribute__((packed))
 # define __expect(condition, x) __builtin_expect(condition, x)
 # define __popcount(x)          __builtin_popcount(x)
 
-#elif defined(Q_OS_WIN) && defined(_MSC_VER)
+#elif defined(WIN32) && defined(_MSC_VER)
 # define __internal
 # define __pure                 __declspec(noalias)
 # define __packed
 # define __expect(condition, x) (condition)
-# define __popcount(x)          (((x&(1<< 0))?1:0)+((x&(1<< 1))?1:0)+((x&(1<< 2))?1:0)+((x&(1<< 3))?1:0)+((x&(1<< 4))?1:0)+((x&(1<< 5))?1:0)+((x&(1<< 6))?1:0)+((x&(1<< 7))?1:0)+\
+# if _MSC_VER < 1600 // 1600 = VS2010
+#  define __popcount(x)         (((x&(1<< 0))?1:0)+((x&(1<< 1))?1:0)+((x&(1<< 2))?1:0)+((x&(1<< 3))?1:0)+((x&(1<< 4))?1:0)+((x&(1<< 5))?1:0)+((x&(1<< 6))?1:0)+((x&(1<< 7))?1:0)+\
                                  ((x&(1<< 8))?1:0)+((x&(1<< 9))?1:0)+((x&(1<<10))?1:0)+((x&(1<<11))?1:0)+((x&(1<<12))?1:0)+((x&(1<<13))?1:0)+((x&(1<<14))?1:0)+((x&(1<<15))?1:0)+\
                                  ((x&(1<<16))?1:0)+((x&(1<<17))?1:0)+((x&(1<<18))?1:0)+((x&(1<<19))?1:0)+((x&(1<<20))?1:0)+((x&(1<<21))?1:0)+((x&(1<<22))?1:0)+((x&(1<<23))?1:0)+\
                                  ((x&(1<<24))?1:0)+((x&(1<<25))?1:0)+((x&(1<<26))?1:0)+((x&(1<<27))?1:0)+((x&(1<<28))?1:0)+((x&(1<<29))?1:0)+((x&(1<<30))?1:0)+((x&(1<<31))?1:0))
+# else
+#  define __popcount(x)         __popcnt(x)
+# endif
 
 #else
-# define __internal
-# define __pure
-# define __packed
-# define __expect
-# define __popcount(x)          (((x&(1<< 0))?1:0)+((x&(1<< 1))?1:0)+((x&(1<< 2))?1:0)+((x&(1<< 3))?1:0)+((x&(1<< 4))?1:0)+((x&(1<< 5))?1:0)+((x&(1<< 6))?1:0)+((x&(1<< 7))?1:0)+\
-                                 ((x&(1<< 8))?1:0)+((x&(1<< 9))?1:0)+((x&(1<<10))?1:0)+((x&(1<<11))?1:0)+((x&(1<<12))?1:0)+((x&(1<<13))?1:0)+((x&(1<<14))?1:0)+((x&(1<<15))?1:0)+\
-                                 ((x&(1<<16))?1:0)+((x&(1<<17))?1:0)+((x&(1<<18))?1:0)+((x&(1<<19))?1:0)+((x&(1<<20))?1:0)+((x&(1<<21))?1:0)+((x&(1<<22))?1:0)+((x&(1<<23))?1:0)+\
-                                 ((x&(1<<24))?1:0)+((x&(1<<25))?1:0)+((x&(1<<26))?1:0)+((x&(1<<27))?1:0)+((x&(1<<28))?1:0)+((x&(1<<29))?1:0)+((x&(1<<30))?1:0)+((x&(1<<31))?1:0))
+# error Platform not supported
 #endif
 
 #endif
