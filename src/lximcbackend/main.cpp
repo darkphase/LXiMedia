cbe91bdd0cf090f8a4331f70d7d580a3149659c8
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2014-07-13 07:23:14 +0000

    Work on port to libvlc and removing Qt.

--- src/lximcbackend/main.cpp
+++ src/lximcbackend/main.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 #include <memory>
 
+#if defined(__unix__)
+# include <clocale>
+#endif
+
 int main(int /*argc*/, const char */*argv*/[])
 {
+#if defined(__unix__)
+  // Ensure UTF-8 is enabled.
+  setlocale(LC_ALL, "");
+#endif
+
   // Allocate these on heap to keep stack free.
   const std::unique_ptr<class messageloop> messageloop(new class messageloop());
   const std::unique_ptr<class backend> backend(new class backend(*messageloop));
---
+++ src/lximcbackend/vlc/media.h
@@ -0,0 +1,59 @@
+/******************************************************************************
+ *   Copyright (C) 2014  A.J. Admiraal                                        *
+ *   code@admiraal.dds.nl                                                     *
+ *                                                                            *
+ *   This program is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License version 3 as        *
+ *   published by the Free Software Foundation.                               *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ ******************************************************************************/
+
+#ifndef VLC_MEDIA_H
+#define VLC_MEDIA_H
+
+#include <string>
+#include <vector>
+
+struct libvlc_media_t;
+
+namespace vlc {
+
+class instance;
+
+class media
+{
+public:
+  enum class track_type { unknown, audio, video, text };
+
+  struct track
+  {
+    track_type type;
+  };
+
+public:
+  media(class instance &, const std::string &path);
+  media(const media &);
+  ~media();
+
+  media & operator=(const media &);
+  inline operator libvlc_media_t *() { return libvlc_media; }
+
+  void parse() const;
+
+  std::string mrl() const;
+  std::vector<track> tracks() const;
+
+private:
+  libvlc_media_t *libvlc_media;
+};
+
+} // End of namespace
+
+#endif
