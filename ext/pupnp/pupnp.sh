07f861f3ad0bfdaad8a9bc84d4a3308f4aa84023
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2014-08-04 21:18:08 +0000

    Work on port to libvlc and removing Qt.

---
+++ ext/pupnp/pupnp.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+PUPNP_VERSION=$3
+
+if [ ! -f $2/libupnp-$3/Makefile ]; then
+    if [ -f $1/libupnp-$3.tar.bz2 ]; then
+        # Extract
+        mkdir $2
+        cp $1/libupnp-$3.tar.bz2 $2
+        cd $2
+        tar -xjf libupnp-$3.tar.bz2
+        cd libupnp-$3
+
+        # Patch
+        patch -p0 < $1/fix-binding-port.patch
+        patch -p0 < $1/fix-repeat-announcement-spread.patch
+        patch -p0 < $1/add-support-cachecontrol-nocache.patch
+        patch -p0 < $1/add-support-multiple-interfaces.patch
+
+        patch -p0 < $1/fix-mingw47-build.patch
+        patch -p0 < $1/fix-windows-xp-compatibility.patch
+
+        # Configure
+        sh configure --enable-static --disable-shared --disable-samples --disable-dependency-tracking CFLAGS="-w -fPIC"
+
+        # Make
+        make
+
+        # Copy public headers
+        mkdir upnp/inc/upnp
+        cp upnp/inc/*.h upnp/inc/upnp
+    fi
+fi
+
+exit
