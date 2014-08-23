be03e89c163b3756456cd460ce9dbaeddf2f1822
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2014-08-23 22:16:23 +0000

    Added Windows build.

--- ext/pupnp/pupnp.sh
+++ ext/pupnp/pupnp.sh
@@ -3,12 +3,12 @@
 PUPNP_VERSION=$3
 
 if [ ! -f $2/libupnp-$3/upnp/inc/upnp/upnpconfig.h ]; then
-    if [ -f $1/libupnp-$3.tar.bz2 ]; then
+    if [ -f $1/libupnp-$3.tar.xz ]; then
         # Extract
         mkdir -p $2
-        cp $1/libupnp-$3.tar.bz2 $2
+        cp $1/libupnp-$3.tar.xz $2
         cd $2
-        tar -xjf libupnp-$3.tar.bz2
+        tar -xJf libupnp-$3.tar.xz
         cd libupnp-$3
 
         # Patch
