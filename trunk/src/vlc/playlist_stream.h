7e919a5d46159021d6b08f30ed162243c4a06e3a
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2014-10-19 15:47:28 +0000

    Added playlist_stream.

---
+++ trunk/src/vlc/playlist_stream.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ *   Copyright (C) 2014  A.J. Admiraal                                        *
+ *   code@admiraal.dds.nl                                                     *
+ *                                                                            *
+ *   This program is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License version 3 as        *
+ *   published by the Free Software Foundation.                               *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ ******************************************************************************/
+
+#ifndef VLC_PLAYLIST_STREAM_H
+#define VLC_PLAYLIST_STREAM_H
+
+#include <istream>
+#include <memory>
+#include <string>
+#include <queue>
+
+namespace vlc {
+
+class playlist_stream : public std::istream
+{
+public:
+    playlist_stream(
+            class instance &,
+            const std::string &transcode,
+            const std::string &mux);
+
+    ~playlist_stream();
+
+    void add(const std::string &mrl);
+
+private:
+    class streambuf;
+
+    class instance &instance;
+    const std::string transcode;
+    const std::string mux;
+    const std::unique_ptr<class streambuf> streambuf;
+
+    std::queue<std::string> items;
+};
+
+} // End of namespace
+
+#endif
