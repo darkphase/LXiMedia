c57ac90a93668d0e2eba933fc76050f3cdb58f2f
aadmiraal <aadmiraal@45753364-5482-4884-9a48-e6ddc0a5f779>
2015-03-07 16:53:01 +0000

    Fixed build script for Dash.

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,10 +3,10 @@
 CURDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 UNAME=`uname`
-if [[ "${UNAME}" == 'Linux' ]]; then
+if [ "${UNAME}" = 'Linux' ]; then
     sh ${CURDIR}/debian/buildsourcepackage.sh || exit 1
     sh ${CURDIR}/debian/build.sh || exit 1
     sh ${CURDIR}/win32/build.sh || exit 1
-elif [[ "${UNAME}" == 'Darwin' ]]; then
+elif [ "${UNAME}" = 'Darwin' ]; then
     sh ${CURDIR}/macx/build.sh || exit 1
 fi
